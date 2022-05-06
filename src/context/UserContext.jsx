c064989e231922133de422168e62ce9af35b3618
Denzel <dbartolaba123@gmail.com>
2022-05-05 17:56:00 -0700

    creating state for my usercontext

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,2 +1,27 @@
 import { createContext, useContext, useState } from 'react';
 import { getUser, signInUser } from '../services/user';
+
+export const UserContext = createContext();
+
+export function UserProvider({ children }) {
+  const currentUser = getUser();
+  const [user, setUser] = useState(currentUser || { email: null });
+
+  async function login(email, password) {
+    const authenticatedUser = await signInUser({ email, password });
+
+    if (authenticatedUser) {
+      setUser(authenticatedUser);
+    }
+  };
+
+  function logout() {
+    setUser({ email: null });
+  };
+
+  return (
+    <UserContext.Provider value={{ user, login, logout }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
