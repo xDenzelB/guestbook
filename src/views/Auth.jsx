7047897ebd63e4c1b3e6be2578d65dbd08a0e9e4
Denzel <dbartolaba123@gmail.com>
2022-05-06 11:15:58 -0700

    set state for my auth

--- src/views/Auth.jsx
+++ src/views/Auth.jsx
@@ -1,7 +1,14 @@
 import { useState } from 'react';
-import { useHistory, useLocation } from 'react-router-dom';
-
+import { Redirect, useHistory } from 'react-router-dom';
+import { useUser } from '../context/UserContext';
+import { signInUser, signUpUser } from '../services/user';
 export default function Auth() {
+  const history = useHistory();
+  const { user, setUser } = useUser();
+  const [signUp, setSignup] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
   return (
     <div>Auth</div>
   )
