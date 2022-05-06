45e68af22c109c502721982940351281f4dfbce8
Denzel <dbartolaba123@gmail.com>
2022-05-06 10:34:18 -0700

    Added in my routes with context wrapped around

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import { Route, Switch } from 'react-router-dom';
 import Auth from './views/Auth';
 import Home from './views/Home';
 import Dashboard from './views/Dashboard';
+import PrivateRoute from './components/PrivateRoute';
+import { UserProvider } from './context/UserContext';
 export default function App() {
   return (
+      <UserProvider>
     <Switch>
       <Route path='/login'>
         <Auth />
       </Route>
-    </Switch>
+      <PrivateRoute path='/dashboard'>
+        <Dashboard />
+      </PrivateRoute>
+      <Route path='/'>
+        <Home />
+      </Route>
+      </Switch>
+      </UserProvider>
   )
 }
